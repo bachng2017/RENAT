bcea216fe17a2723607d280d91ef11f911abeb3e
jenkins <bachng@gmail.com>
2020-01-05 14:24:45 +0900

    add -r to tools/ignore.sh to include sub items

--- tools/ignore.sh
+++ tools/ignore.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+# -*- coding: utf-8 -*-
 # 
 #
 #
 BASE=$(basename $0)
 if [ $# -lt 2 ];  then
-  echo "usage: $BASE -l <base>"
-  echo "       $BASE -d <item>"
-  echo "       $BASE <item> <comment>"
+  echo "Mark an item with ignore flag that would be ignored by run.sh. Option -r will mark the item and all its sub items"
+  echo "Usage: "
+  echo "  $BASE -l base:             show the ignore list of the base folder"
+  echo "  $BASE -d [-r] item:        remove ignore flag of an item"
+  echo "  $BASE [-r] item comment:   mark an item as ignore with comment"
   exit 1
 fi
 
@@ -21,26 +24,37 @@ if [ "$1" == "-l" ]; then
     done
     echo ""
 elif [ "$1" == "-d" ]; then
-    ITEM=$2
-    if [ -f ./$ITEM/.ignore ]; then
-        rm -f ./$ITEM/.ignore
-        echo "removed .ignore in '$ITEM'"
-    else
-        echo "could not find .ignore in '$2'"
-        exit 1
+  if [ "$2" == "-r" ]; then
+    RECUR=""
+    ITEM=$3
+  else
+    RECUR="-maxdepth=0"
+    ITEM=$2 
+  fi 
+  for TARGET in $(find $ITEM $RECUR -type d); do
+    if [ -f $TARGET/.ignore ]; then
+      rm -f $TARGET/.ignore
+      echo "removed .ignore in '$TARGET'"
     fi
+  done
 else
+  if [ "$1" == "-r" ]; then
+    ITEM=$2
+    COMMENT=$3
+    RECUR=""
+  else
     ITEM=$1
     COMMENT=$2
-    if [ -d ./$ITEM ]; then
-        if [ -f ./$ITEM/.ignore ];  then
-            OLD_COMMENT=$(cat ./$ITEM/.ignore)
-            echo "old comment was: $OLD_COMMENT"
-        fi
-        echo "$COMMENT" > ./$ITEM/.ignore
-        echo "ignored item '$ITEM'"
-    else
-        echo "could not find '$1'"
-        exit 1
+    RECUR="-maxdepth 0"
+  fi
+
+  for TARGET in $(find $ITEM $RECUR -type d); do
+    if [ -f $TARGET/main.robot ]; then
+      if [ -f $TARGET/.ignore ];  then
+          OLD_COMMENT=$(cat $TARGET/.ignore)
+      fi
+      echo "$COMMENT" > $TARGET/.ignore
+      echo "ignored item '$TARGET'"
     fi
+  done
 fi
